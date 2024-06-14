87732b42257900f3e2ace65eb8a4419c51cc5933
Manuel <dinardomanuel25@gmail.com>
2024-06-14 10:17:53 +0200

    secondo commit per classe email

---
+++ Email.java
@@ -0,0 +1,8 @@
+public class Email extends Notifica{
+
+    public Email(String destinatario, String messaggio) {
+        super(destinatario, messaggio);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
