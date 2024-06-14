531d0f8509d9de8f08ead664b01a980d5b5c24e8
Manuel <dinardomanuel25@gmail.com>
2024-06-14 10:17:01 +0200

    primoCommit classe astratta Notifica

---
+++ Notifica.java
@@ -0,0 +1,16 @@
+public abstract class Notifica{
+    protected String destinatario;
+    protected String messaggio;
+
+
+    public Notifica(String destinatario, String messaggio) {
+        this.destinatario = destinatario;
+        this.messaggio = messaggio;
+    }
+
+    public void invia(){
+        
+    }
+
+    
+}
